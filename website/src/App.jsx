cd79c58a477910c5e8287cae5d439cd0e2895c82
Mutombe <simbarashemutombe1@gmail.com>
2024-12-06 15:49:38 +0200

    ...

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes,useLocation, Route } from "react-router-dom";
 import { Toaster } from "sonner";
 import Navbar from "./components/nav/nav";
 import Home from "./components/home/home";
@@ -8,11 +8,23 @@ import Booking from "./components/booking/booking";
 import Footer from "./components/footer/footer";
 import Contact from "./components/contact/contact";
 import About from "./components/about/about";
+import { useEffect } from "react";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen bg-neutral-50">
+        <ScrollToTop />
         <Navbar />
         <main className="flex-grow">
           <Routes>
